f93d14846abfd49ed032b4748ee4a24a09dabcc9
ArvinSamadabadi <69974729+ArvinSamadabadi@users.noreply.github.com>
2020-11-20 14:12:05 -0500

    Update Real Estate Sold or Not.py
    -Imported Linear Regression, Stat Models, and the Metric Score in order to implement the code.

--- Real Estate Sold or Not.py
+++ Real Estate Sold or Not.py
@@ -1,1 +1,11 @@
-
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+import statsmodels.api as sm
+from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.datasets import make_blobs
+from sklearn.naive_bayes import GaussianNB
